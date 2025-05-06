73ee59571e525ad1daff8cc88a61ab48efa81baf
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-05-06 14:42:26 +0600

    added api for loaded single data from database

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -31,6 +31,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   
+    const jobsCollection = client.db("jobPortal").collection("jobs");
+    
+    app.get('/jobs', async(req, res)=>{
+        const cursor = jobsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+   app.get('/jobs/:id', async (req, res)=>{
+    const id = req.params.id
+    const query = { _id: new ObjectId(id)}
+    const result = await jobsCollection.findOne(query)
+    res.send(result)
+   })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
